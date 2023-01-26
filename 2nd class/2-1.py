cd31fe39da27f83f5d6aa8fef400e6b56f3c2c51
bamse <bamse1517@gmail.com>
2023-01-26 20:49:49 +0900

    4차시

---
+++ 2nd class/2-1.py
@@ -0,0 +1,31 @@
+# print(10 * 5 / 2) 
+
+# print(20 * 10 / 2)
+# print(100 * 38 / 2)
+# print(47 * 22 / 2)
+
+# width = 10
+# height = 5
+# print(width * height / 2)
+
+# width = 100
+# print(width)
+
+# print(10 * 10 * 3.14)
+
+# pi = 3.14
+# radius = 10
+# print(radius * radius * pi)
+
+# pi = 3.1415926535
+# radius = 10
+# print(radius * radius * pi)
+
+# sentence = 'hello world!'
+# print(sentence)
+
+# sentence = ''Good day' he tought.'
+# print(sentence)
+
+sentence = "'Good day' he tought."
+print(sentence)
