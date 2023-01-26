a61db695a46b5deafad0d76167b9969ae97136ca
bamse <bamse1517@gmail.com>
2023-01-26 21:42:40 +0900

    2주차 퀴즈

---
+++ 2nd class/2nd-quiz.py
@@ -0,0 +1,10 @@
+# 문제
+# width = input("밑변의 값 : ")
+# height = input("높이의 값 : ")
+# area = width * height
+# print(f"삼각형의 넓이는 {area} 입니다.")
+
+width = int(input("밑변의 값 : "))
+height = int(input("높이의 값 : "))
+area = width * height / 2
+print(f"삼각형의 넓이는 {area} 입니다.")
