71d2e19e2e87625c66214f17d00a3a0f10aaa352
bamse <bamse1517@gmail.com>
2023-01-26 21:16:49 +0900

    5차시

---
+++ 2nd class/2-2.py
@@ -0,0 +1,63 @@
+# three = 3
+# three3 = 3
+# three_3 = 3
+# print(three)
+# print(three3)
+# print(three_3)
+# Three = 4
+# print(Three)
+# _three = 3
+# 3three = 3
+# print(3three)
+# three 3 = 3
+# print(three 3)
+# print = 3
+# print(print)
+
+# a = 10
+# b = 3.14
+# c = a * a * b
+# print(c)
+
+# radius = 10
+# pi = 3.14
+# area = radius * radius * pi
+# print(area)
+
+# 파스칼 표기법 PascalCase
+# radius = 10
+# pi = 3.14
+# Circlearea = radius * radius * pi
+# print(Circlearea)
+
+# 카멜 표시법 camelCase
+# radius = 10
+# pi = 3.14
+# circleArea = radius * radius * pi
+# print(circleArea)
+
+# 스네이크 표기법 snake_case
+# radius = 10
+# pi = 3.14
+# circle_area = radius * radius * pi
+# print(circle_area)
+
+w = 10
+h = 5
+a = w * h /2
+print(f"너비 {w}cm, 높이 {h}cm 인 사각형의 넓이는 {a}cm 입니다.")
+
+width = 10
+height = 5
+rect_area = width * height /2
+print("너비 {width}cm, 높이 {height}cm 인 사각형의 넓이는 {rect_area}cm 입니다.")
+
+width = 10
+height = 5
+rect_area = width * height /2
+print("너비 %dcm, 높이 %dcm 인 사각형의 넓이는 %dcm 입니다." % (width, height, rect_area))
+
+width = 10
+height = 5
+rect_area = width * height /2
+print("너비 {0}cm, 높이 {1}cm 인 사각형의 넓이는 {2}cm 입니다.".format(width, height, rect_area))
