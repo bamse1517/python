39fae6dd4a836d12a822d8617ab014252eb917ea
bamse <bamse1517@gmail.com>
2023-01-26 23:35:45 +0900

    3주차 퀴즈

---
+++ 3rd class/3rd-quiz.py
@@ -0,0 +1,8 @@
+# 퀴즈
+# num_1, num_2, num_3 = input("input number : ")
+# print(num_1 + num_2 + num_3)
+
+# 답
+str_1, str_2, str_3 = input("input number : ").split()
+num_1, num_2, num_3 = int(str_1), int(str_2), int(str_3)
+print(num_1 + num_2 + num_3)
