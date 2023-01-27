78b94f0d33142832da43403cb5b2f8473590b794
bamse <bamse1517@gmail.com>
2023-01-27 19:23:41 +0900

    10차시

---
+++ 4th class/4-1.py
@@ -0,0 +1,56 @@
+# ord("a")
+# chr(97)
+
+# ord("가")
+
+# chr(ord("가") + 1176)
+
+# print("hello world!")
+
+# print("hello world")
+# print("hello\nworld")
+# print("hello\tworld")
+
+# print("\"hello world\"")
+# print("\'hello world\'")
+# print("'hello world'")
+# print("'hello \\world'")
+
+# sentence = '"The best way predict the future is to invent it." \n- Alan kay'
+# print(sentence)
+
+# sentence = '''
+# The best way predict the future is to invent it.
+# - Alan kay
+# '''
+# print(sentence)
+
+# str_1 = "hello"
+# str_2 = "world"
+# print(str_1 + str_2)
+# print(str_1 + ' ' + str_2)
+
+# print(str_1 + 3)
+
+# str_1 = "python"
+# print(str_1 * 3)
+
+# 문자열에 안되는거
+# str_1 = "python"
+# print(str_1 * str_1)
+
+# 문자열에 안되는거
+# str_1 = "python"
+# print(str_1 - 3)
+
+# 문자열에 안되는거
+# str_1 = "python"
+# print(str_1 / 3)
+
+str_1 = "python_"
+str_2 = "python"
+print(str_1 * 99 + str_2)
+
+str_1 = "python"
+str_2 = "-"
+print((str_1 + str_2) * 99 + str_1)
