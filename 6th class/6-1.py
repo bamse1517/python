6c01b7ed27abfecc739c04eabaac72604951e2ec
bamse <bamse1517@gmail.com>
2023-01-29 23:09:56 +0900

    16차시

--- 6th class/6-1.py
+++ 6th class/6-1.py
@@ -1,10 +1,10 @@
-# #@title 삼각형 면적을 구하는 코드
+# 삼각형 면적을 구하는 코드
 # width = 10
 # height = 5
 # area = width * height / 2
 # print(f"삼각형의 면적은 {area} 입니다.")
 
-#@title 도형 면적을 구하는 코드
+# 도형 면적을 구하는 코드
 width = int(input("너비를 입력하세요: "))
 height = int(input("높이를 입력하세요: "))
 figure = input("삼각형 또는 사각형을 입력하세요: ")
