f464f55887eed6d4f432de858fe43daddc5ab645
bamse <bamse1517@gmail.com>
2023-01-29 23:14:14 +0900

    17차시

--- 5th class/5th-quiz.py
+++ 5th class/5th-quiz.py
@@ -2,6 +2,7 @@
 # age = int(input("만 나이를 입력하세요: "))
 # age < 12 and age > 65
 
+
 # 정답
 age = int(input("만 나이를 입력하세요: "))
 age <= 12 or age >= 65
--- 6th class/6-2.py
+++ 6th class/6-2.py
@@ -1,4 +1,4 @@
-# #@title 민수는 청소년인가?
+# # 민수는 청소년인가?
 # age = 20
 # print(9 <= age)
 # print(age <= 23)
@@ -6,31 +6,31 @@
 # age = 20
 # (9 <= age) and (age <= 23)
 
-# #@title 암산하기
+# # 암산하기
 # print("3 * 5 = 15?")
 # 3 * 5 == 14
 
-# #@title 3의 배수인가?
+# # 3의 배수인가?
 # x = 10
 # x % 3 == 0
 
-# #@title 3과 5의 공배수인가?
+# # 3과 5의 공배수인가?
 # x = 9
 # (x % 3 == 0) and (x % 5 == 0)
 
-# #@title 민수는 청소년인가?
+# # 민수는 청소년인가?
 # age = 50
 
 # if (9 <= age) and (age <= 24):
 #   print("청소년입니다.")
 
-#   #@title 입력한 숫자는 3의 배수인가?
+# # 입력한 숫자는 3의 배수인가?
 # x = int(input("x 값을 입력하세요: "))
 
 # if x % 3 == 0:
 #   print("3의 배수입니다!")
 
-#   #@title 어떤 숫자가 더 클까?
+# # 어떤 숫자가 더 클까?
 # #if문
 # x = int(input("x 값을 입력하세요:"))
 # y = int(input("y 값을 입력하세요:"))
@@ -38,7 +38,7 @@
 # if x > y:
 #   print(f"{x}가 {y}보다 더 큽니다.")
 
-#   #@title 암산하기
+# # 암산하기
 # import random
 
 # num_1, num_2 = random.randint(1, 10), random.randint(1, 10)
@@ -48,14 +48,14 @@
 # if result == input_result:
 #   print("정답입니다!")
 
-#   #@title 민수는 청소년인가?
+# # 민수는 청소년인가?
 # age = 20
 # if (9 <= age) and (age <= 24):
 #   print("청소년 입니다.")
 # else:
 #   print("청소년이 아닙니다.")
 
-#   #@title 입력한 숫자는 3또는 5의 배수인가?
+# # 입력한 숫자는 3또는 5의 배수인가?
 # x = int(input("x 값을 입력하세요: "))
 
 # if (x % 3 == 0) or (x % 5 == 0):
@@ -63,7 +63,7 @@
 # else:
 #   print("3또는 5의 배수가 아닙니다!")
 
-  #@title 암산하기
+# 암산하기
 import random
 
 num_1, num_2 = random.randint(1, 10), random.randint(1, 10)
