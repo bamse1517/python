a8107a47276b5d274c0820eea7296ff135843296
bamse <bamse1517@gmail.com>
2023-02-01 23:39:19 +0900

    8주차 퀴즈

---
+++ 8th class/8th-quiz.py
@@ -0,0 +1,14 @@
+# 2번 문제
+endic = {"apple":"사과", "banana": "바나나", "fineapple":"파인애플"}
+print(endic)
+
+# 3번 문제
+# # 오류 코드
+# grade = ('a', 'b', 'f')
+# grade.insert(2, 'c')
+# print(grade)
+
+# 정답코드
+grade = ['a', 'b', 'f']
+grade.insert(2, 'c')
+print(grade)
