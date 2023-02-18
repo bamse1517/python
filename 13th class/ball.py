df4f329603752303ad60a215a90ebe75ca9e0af5
bamse <bamse1517@gmail.com>
2023-02-18 22:54:03 +0900

    13차시 자료

---
+++ 13th class/ball.py
@@ -0,0 +1,38 @@
+import pygame
+from random import randint
+
+BLACK = (0,0,0)
+
+class Ball(pygame.sprite.Sprite):
+  def __init__(self, color, width, height):
+    super().__init__()
+
+    self.image = pygame.Surface([width, height])
+    self.image.fill(BLACK)
+    self.image.set_colorkey(BLACK)
+    pygame.draw.rect(self.image, color, [0, 0, width, height])
+    self.velocity = [randint(4,8), randint(-8,8)]
+    self.rect = self.image.get_rect()
+
+  def update(self):
+    self.rect.x += self.velocity[0]
+    self.rect.y += self.velocity[1]
+
+  def left_right_turn(self):
+    self.velocity[0] = -self.velocity[0]
+
+  def up_down_turn(self):
+    self.velocity[1] = -self.velocity[1]
+
+  def bounce(self):
+    self.left_right_turn()    
+    self.velocity[1] = randint(-8,8)
+
+  def new(self):
+    self.rect.x = 345
+    self.rect.y = 195
+    self.velocity[0] = -self.velocity[0]
+    self.velocity[1] = randint(-8,8)
+
+  def stop(self):
+    self.velocity = [0, 0]
